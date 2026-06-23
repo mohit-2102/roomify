37a306d72eacf2a4ee440377b392d12ceb3864f7
mohit-2102 <mohit.mathangi@gmail.com>
2026-06-23 13:56:07 +0530

    Project setup an handled AUhtentication

---
+++ roomify/components/Navbar.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Box } from "lucide-react";
+import Button from './ui/Button';
+import { useOutletContext } from 'react-router';
+
+const Navbar = () => {
+
+  const { isSignedIn, userName, signOut, signIn } = useOutletContext<AuthContext>()
+
+  const handleAuthClick = async () => {
+    if (isSignedIn) {
+      try{
+        await signOut();
+      } catch(e) {
+        console.error('Error signing out:', e);
+      }
+
+      return
+    }
+
+    try {
+      await signIn()
+    } catch (e) {
+      console.error('Error signing in:', e);  
+    }
+  }
+
+  return (
+    <header className='navbar'>
+      <nav className='inner'>
+        <div className="left">
+          <div className='brand'>
+            <Box className='logo' />
+            <span className="name">Roomify</span>
+          </div>
+          <ul className='links'>
+            <a href="#">Product</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
+
+        <div className="actions">
+          {isSignedIn ? (
+            <>
+              <span className='greeting'>
+                {userName ? `Hi, ${userName}` : 'Hi, User'}
+              </span>
+              <Button size='sm' onClick={handleAuthClick} className='btn'>
+              Log Out
+              </Button>
+            </>
+
+          ): (
+              <>
+              <Button size='sm' onClick = { handleAuthClick }  variant='ghost'>
+              LogIn
+            </Button>
+            <a href="#upload" className='cta'>
+            Get Started
+            </a>
+      </>
+      )
+          }
+    </div>
+    </nav >
+
+    </header >
+  )
+}
+
+export default Navbar
